40d3427c718aea8121e9b716e9fd1201fbc625b0
davemac93 <112555280+davemac93@users.noreply.github.com>
2022-11-24 14:47:08 +0100

    Update mergeSortWithInsertionSort.py

--- mergeSortWithInsertionSort.py
+++ mergeSortWithInsertionSort.py
@@ -1,12 +1,23 @@
 import time
 import random
 from array import *
-
+def insertionSort(arr):
+ 
+    for i in range(1, len(arr)):
+ 
+        key = arr[i]
+        
+        j = i-1
+        while j >=0 and key < arr[j] :
+                arr[j+1] = arr[j]
+                j -= 1
+        arr[j+1] = key
 def merge_sort(arr):
     if len(arr) > 1:
+       if len(arr) > 1:
         left_arr = arr[:len(arr)//2]
         right_arr = arr[len(arr)//2:]
-         
+        
         #recursion
         merge_sort(left_arr)
         merge_sort(right_arr)
@@ -36,42 +47,9 @@ def merge_sort(arr):
 
 def merge_sort_with_insertion_sort(arr, n):
     if n < len(arr):
-        if len(arr) > 1:
-            left_arr = arr[:len(arr)//2]
-            right_arr = arr[len(arr)//2:]
-        
-            #recursion
-            merge_sort(left_arr)
-            merge_sort(right_arr)
-
-            #merge
-            i = 0 #left_arr idx
-            j = 0 #right_arr idx
-            k = 0 #merged array idx
-        while i < len(left_arr) and j < len(right_arr):
-            if left_arr[i] < right_arr[j]:
-                arr[k] = left_arr[i]
-                i += 1
-            else:
-                arr[k] = right_arr[j]
-                j += 1
-                k += 1
-        
-        while i < len(left_arr):
-            arr[k] = left_arr[i]
-            i += 1
-            k += 1
-
-        while j < len(right_arr):
-            arr[k] = right_arr[j]
-            j += 1
-            k += 1
+        merge_sort(arr)
     else:
-        for i in range(1, len(arr)):
-            j = i
-            while arr[j-1] > arr[j] and j > 0:
-                arr[j-1], arr[j] = arr[j], arr[j-1]
-                j -= 1
+        insertionSort(arr)
 
 def randomArray():
     arr = []
@@ -81,8 +59,8 @@ def randomArray():
         arr.append(x)
     return arr
 
-n = int(input("Enter the highest number for Insertion Sort else it will be soreted by Merge Sort: "))
 arr = randomArray()
+n = int(input("Enter the highest number for Insertion Sort else it will be soreted by Merge Sort: "))
 print("Before:", arr)
 st = time.time()
 merge_sort_with_insertion_sort(arr, n)
